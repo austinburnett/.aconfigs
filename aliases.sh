6d52e1118fdea1b61ae38067f3a8360458daefe1
austinburnett <aburn1011@gmail.com>
2026-09-12 14:31:06 -0400

    Add vdiff,notes,configs aliases

--- aliases.sh
+++ aliases.sh
@@ -1,14 +1,24 @@
+if [ -f $HOME/.aconfigs/myenv.env ]; then
+    source $HOME/.aconfigs/myenv.env
+else
+    echo "File 'myenv.env' not found at $HOME/.aconfigs/myenv.env. Aliases will fail."
+fi
+
+
 ################################################################################
 # Directory Shortcuts
-################################################################################
-alias repos="cd ~/repos/"
 
-# Creates a directory and jumps into it.
-mkcd()
-{
-    mkdir -p "$1" && cd "$1"
-}
+alias projects="cd ${HOME}/projects/repos/"
+alias sandbox="cd ${HOME}/sandbox"
+alias configs="cd ${HOME}/.aconfigs/"
+
+alias notes="nvim '${PATH_NOTES}'"
+
 
 ################################################################################
 # Git Shortcuts
-################################################################################
+
+# TODO:
+# I could extend this to be a shell program such that you could do "vdiff -p"
+# for a diff between HEAD and previous.
+alias vdiff="git difftool --dir-diff --tool meld &"
